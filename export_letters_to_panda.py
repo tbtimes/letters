342c0a8c213b3300bb91ef6a3fcc34c37785a8f7
bhiggins <higs4281@gmail.com>
2014-09-25 14:36:32 -0400

    add entry parser to handle different directory forms

--- export_letters_to_panda.py
+++ export_letters_to_panda.py
@@ -6,10 +6,12 @@ from csvkit import CSVKitDictReader as cdr
 from django.template.defaultfilters import slugify
 from ftplib import FTP
 import datetime
+from dateutil import parser
 
 # PASSWORDS AND KEYS ARE STORED IN ENVIRONMENT VARIABLES
 # TO USE OUTSIDE ITS HOME ENVIRONMENT, THESE VARIABLES WILL HAVE TO BE SET MANUALLY
 USER = os.getenv('PANDA_USER')
+FTP_USER = "ad/%s" % os.getenv('USER')
 PANDA_KEY = os.getenv('PANDA_API_KEY')
 FTP_LOGON = os.getenv('TIMES_LOGON')
 
@@ -42,7 +44,37 @@ LETTERS_FTP = 'hn-dti02'
 FFOLDERS = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900]
 processed = 0
 put_data = {'objects': []}
-ftp = FTP(LETTERS_FTP, USER, FTP_LOGON)
+ftp = FTP(LETTERS_FTP, FTP_USER, FTP_LOGON)
+
+def parse_entry(entry):
+    """
+    parses entry in either form: 
+    ['-rwxrwxrwx', '1', 'owner', 'group', '666', 'Nov', '4', '22:27', '67']
+    OR
+    ['09-17-14', '09:21PM', '317', '10']
+    """
+    if len(entry) == 9:
+        if ":" in entry[7]:
+            TIME = entry[7]
+        else:
+            TIME = '00:00'                    
+        dateraw = "%s %s %s %s" % (year, entry[5], entry[6], TIME)
+        try:
+            dater = datetime.datetime.strptime(dateraw, "%Y %b %d %H:%M")
+        except:
+            print "couldn't parse date %s" % dateraw
+            DATE = None
+        else:
+            DATE = dater.strftime("%Y-%m-%d %I:%M%p")
+        return DATE
+    elif len(entry) == 4:
+        dateraw = "%s %s" % (entry[0], entry[1])
+        try: 
+            DATE = parser.parse(dateraw)
+        except:
+            print "couldn't parse date %s" % dateraw
+            DATE = None
+        return DATE
 
 for fold in FFOLDERS:
     PATH = "/External input/archive/%s/" % fold
@@ -59,16 +91,21 @@ for fold in FFOLDERS:
         for each in postlines:
             entries.append([bit.strip() for bit in each.split(' ') if bit.strip()])
         # for entry in entries:# ['07-31-13', '02:13PM', '1665', '0']
-        for entry in entries:# ['-rwxrwxrwx', '1', 'owner', 'group', '666', 'Nov', '4', '22:27', '67']
+        for entry in entries:# ['-rwxrwxrwx', '1', 'owner', 'group', '666', 'Nov', '4', '22:27', '67'] 
             processed += 1
             body = []
             ftp.retrlines('RETR %s' % entry[-1], body.append)
             if body and body[0].startswith('#SOHn=LET') and 'a=' in body[0]:# we have a letter, with a name!
                 # dateraw = "%s %s" % (entry[0].strip(), entry[1].strip())
-                year = datetime.datetime.now().year
-                dateraw = "%s %s %s %s" % (year, entry[5], entry[6], entry[7])
-                dater = datetime.datetime.strptime(dateraw, "%Y %b %d %H:%M")
-                DATE = dater.strftime("%Y-%m-%d %I:%M%p")
+                NOW = datetime.datetime.now()
+                year = NOW.year
+                if NOW.month ==1 and entry[5] == 'Dec':
+                    year = year-1
+                dater = parse_entry(entry)
+                if dater:
+                    DATE = dater.strftime("%Y-%m-%d %I:%M%p")
+                else:
+                    DATE = ''
                 metaline = body[0].replace('#SOHn=LET', '').replace('#STX', '')
                 if 'k=' in metaline:
                     split1 = metaline.split('k=')[1].strip()
